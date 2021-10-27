f021cb4bb3988d2f078a6227c77b20fb0d68a055
Marcos <marcosmartinezpalermo@gmail.com>
2021-10-27 01:01:07 -0300

    Changed structure for a multiple commands one and started working on crypto retrieving

--- functions.py
+++ functions.py
@@ -9,6 +9,8 @@ from datetime import datetime
 from requests import get
 from bs4 import BeautifulSoup
 
+commands = ['!dolar', '!dólar', '!cripto']
+
 def reset_timer():
   return time()
 
@@ -16,7 +18,7 @@ def init_praw():
   return Reddit(
     client_id = environ['CLIENT_ID'],
     client_secret = environ['CLIENT_SECRET'],
-    user_agent="console:dolar-bot:v1.1.0 (by u/dolar-bot)",
+    user_agent="console:dolar-bot:v2.0.0 (by u/dolar-bot)",
     username = "dolar-bot",
     password = environ['PASSWORD']
   )
@@ -28,7 +30,7 @@ def log_error(message):
 def post_have_comments(post):
   return (post.num_comments > 0)
 
-def AlreadyReplied(replies):
+def already_replied(replies):
   for reply in replies:
     if reply.author.name == "dolar-bot":
       return True
@@ -63,7 +65,34 @@ def get_dolar_values():
   return [dolar_oficial_compra, dolar_oficial_venta, dolar_blue_compra, dolar_blue_venta, 
   dolar_bolsa_compra, dolar_bolsa_venta, dolar_ccl_compra, dolar_ccl_venta, dolar_solidario_venta, dolar_turista_venta]
 
-def reply_comment(comment):
+def get_cripto_values():
+  bitcoin_page = get("https://www.ripio.com/ar/bitcoin/cotizacion/")
+  ethereum_page = get("https://www.ripio.com/ar/ethereum/cotizacion/")
+  dai_page = get("https://www.ripio.com/ar/dai/cotizacion/")
+  usdc_page = get("https://www.ripio.com/ar/usdc/cotizacion/")
+
+  bitcoin_soup = BeautifulSoup(bitcoin_page.content, "html.parser")
+  ethereum_soup = BeautifulSoup(ethereum_page.content, "html.parser")
+  dai_soup = BeautifulSoup(dai_page.content, "html.parser")
+  usdc_soup = BeautifulSoup(usdc_page.content, "html.parser")
+
+  bitcoin_compra = bitcoin_soup.select('--buy')[0].string
+  bitcoin_venta = bitcoin_soup.select('--price')[1].string
+  ethereum_compra = ethereum_soup.select('--price')[0].string
+  ethereum_venta = ethereum_soup.select('--price')[1].string
+  dai_compra = dai_soup.select('--price')[0].string
+  dai_venta = dai_soup.select('--price')[1].string
+  usdc_compra = usdc_soup.select('--price')[0].string
+  usdc_venta = usdc_soup.select('--price')[1].string
+
+
+  return [bitcoin_compra, bitcoin_venta, ethereum_compra, ethereum_venta, dai_compra, dai_venta, usdc_compra, usdc_venta]
+
+def reply_comment(comment, reply):
+  comment.reply(reply)
+  sleep(5)
+
+def generate_dolar_reply():
   dolar_values = get_dolar_values() 
 
   reply = """
@@ -83,18 +112,47 @@ def reply_comment(comment):
   ^(Feedback? Bugs?: )[^(Github)](https://github.com/marcosmarp/dolar-bot)
   """
 
-  reply = reply.format(dolar_values[0], dolar_values[1], dolar_values[2], dolar_values[3], dolar_values[4], 
+  return reply.format(dolar_values[0], dolar_values[1], dolar_values[2], dolar_values[3], dolar_values[4], 
   dolar_values[5], dolar_values[6], dolar_values[7], dolar_values[8], dolar_values[9], 
   datetime.now(pytz.timezone('America/Argentina/Buenos_Aires')).strftime("%d/%m/%Y %H:%M:%S"))
 
-  comment.reply(reply)
-  sleep(5)
+def generate_cripto_reply():
+  cripto_values = get_cripto_values() 
+
+  reply = """
+  |Divisa|Compra|Venta|
+  |:-|:-|:-|
+  |**Bitcoin**|AR{0}|AR{1}|
+  |**Ethereum**|AR{2}|AR{3}|
+  |**DAI**|AR{4}|AR{5}|
+  |**USDC**|AR{6}|AR{7}|'
+
+  Información actualizada al {8} desde [ripio](https://www.ripio.com/ar/criptomonedas/cotizacion/)
+  
+  ^(Soy un bot y esta acción fue realizada automáticamente)
+  
+  ^(Feedback? Bugs?: )[^(Github)](https://github.com/marcosmarp/dolar-bot)
+  """
+
+  return reply.format(cripto_values[0], cripto_values[1], cripto_values[2], cripto_values[3], cripto_values[4], 
+  cripto_values[5], cripto_values[6], cripto_values[7], datetime.now(pytz.timezone('America/Argentina/Buenos_Aires')).strftime("%d/%m/%Y %H:%M:%S"))
 
 def inform_reply_on_screen(comment):
   now = datetime.now(pytz.timezone('America/Argentina/Buenos_Aires'))
   dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
   print("           " + dt_string + ": replied " + comment.author.name + "'s comment", file=stderr)
   print("---------------", file=stderr)
+
+def check_for_command(comment):
+  for command in commands:
+    if command in comment.body.lower():
+      return True
+  return False
+
+def get_command(comment):
+  for command in commands:
+    if command in comment.body.lower():
+      return command
 
 def check_comments(comments):
   for comment in comments:
@@ -104,24 +162,30 @@ def check_comments(comments):
       if not hasattr(comment, "body"):
         log_error("Empty comment")
         continue
-      print("     Comment have body", file=stderr)
+      print("   Comment have body", file=stderr)
 
       if comment.author is None:
         log_error("Comment deleted")
         continue
       print("   Checking " + comment.author.name + "'s comment", file=stderr)
 
-      if not "!dolar" in comment.body.lower() and not "!dólar" in comment.body.lower():
-        log_error("Comment doesn't mention '!dolar' or '!dólar'")
+      if not check_for_command(comment):
+        log_error("Comment doesn't mentions a command'")
         continue
-      print("       Comment mentions '!dolar' or '!dólar'", file=stderr)
+      command = get_command(comment)
+      print("       Comment mentions " + command, file=stderr)
 
-      if AlreadyReplied(comment.replies):
+      if already_replied(comment.replies):
         log_error("Comment already replied")
         continue
       print("         Comment yet to be replied", file=stderr)
-      reply_comment(comment)
+
+      if command == "!dolar" or command == "!dólar":
+        reply_comment(comment, generate_dolar_reply())
+      elif command == "!cripto":
+        reply_comment(comment, generate_cripto_reply())    
       inform_reply_on_screen(comment)
+
       return
 
 def check_new_posts(posts):
@@ -137,8 +201,6 @@ def check_new_posts(posts):
     print(" Post have comments", file=stderr)
 
     check_comments(post.comments)
-
-    
 
 def run_bot(subreddit_handler):
   check_new_posts(subreddit_handler.new(limit=15))
