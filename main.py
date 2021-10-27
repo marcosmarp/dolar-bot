090d1b08546ad6abdb80d2f55a9d4244b465cd5e
Marcos <marcosmartinezpalermo@gmail.com>
2021-10-27 18:29:46 -0300

    Added merval subreddit

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from traceback import print_exc
 
 reddit_handler = init_praw()
 argentina_subreddit = reddit_handler.subreddit("argentina")
+merval_subreddit = reddit_handler.subreddit("merval")
 test_subreddit = reddit_handler.subreddit("dolarbot")
 
 while(True):
   try:
     run_bot(argentina_subreddit)
+    run_bot(merval_subreddit)
   except KeyboardInterrupt: # For quitting with ctrl+C
     break
   except:
