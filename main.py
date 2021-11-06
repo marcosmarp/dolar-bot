df9351855cc1d5b6775b297b150c4c4559fd1e7e
Marcos <marcosmartinezpalermo@gmail.com>
2021-11-06 01:23:47 -0300

    Added r/dankargentina subreddit

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from traceback import print_exc
 reddit_handler = init_praw()
 argentina_subreddit = reddit_handler.subreddit("argentina")
 merval_subreddit = reddit_handler.subreddit("merval")
+dankargentina_subreddit = reddit_handler.subreddit("dankargentina")
 test_subreddit = reddit_handler.subreddit("dolarbot")
 
 while(True):
   try:
     run_bot(argentina_subreddit)
     run_bot(merval_subreddit)
+    run_bot(dankargentina_subreddit)
   except KeyboardInterrupt: # For quitting with ctrl+C
     break
   except:
